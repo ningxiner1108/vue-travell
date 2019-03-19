c16186496e16a27f81d731bb90e858d22579e051
liaohui <1251691629@qq.com>
2019-03-19 23:43:52 +0800

    单页应用

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,5 +1,10 @@
+<!-- template只能向外暴露一个标签，所有只能在最外层包裹一个标签 -->
+<!-- 单页应用，由js进行渲染，优点：界面切换快，缺点：首屏时间稍慢，seo差 -->
 <template>
-	<div>home</div>
+	<div>
+		<div class="home">home</div>
+		<router-link  to="/list" class="home">列表页</router-link>
+	</div>
 </template>
 
 <script>
@@ -7,3 +12,8 @@ export default {
 	name: 'home'
 }
 </script>
+<style>
+	.home{
+		font-size:50px;
+	}
+</style>
